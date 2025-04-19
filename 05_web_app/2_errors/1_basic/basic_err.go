c22bc77d68c42aca1daa9b4a3ffd4c334ad99ad2
Grigorii Gusev <g.gusev@vk.team>
2025-04-19 04:09:57 +0300

    NOTASK: update lecture 5

--- 05_web_app/2_errors/1_basic/basic_err.go
+++ 05_web_app/2_errors/1_basic/basic_err.go
@@ -6,9 +6,26 @@ import (
 	"time"
 )
 
-var (
-	client = http.Client{Timeout: time.Duration(time.Millisecond)}
-)
+var client = http.Client{Timeout: 1 * time.Duration(time.Millisecond)}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("starting server at :8080")
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	err := getRemoteResource()
+	if err != nil {
+		fmt.Printf("error happened: %+v\n", err)
+		http.Error(w, "internal error", 500)
+
+		return
+	}
+
+	w.Write([]byte("all is OK"))
+}
 
 func getRemoteResource() error {
 	url := "http://127.0.0.1:9999/pages?id=123"
@@ -19,27 +36,10 @@ func getRemoteResource() error {
 		// return err
 
 		// будет `res error: time out`. а где?
-		// return fmt.Errof("getRemoteResource: %+v", err)
+		// return fmt.Errorf("getRemoteResource: %w", err)
 
 		return fmt.Errorf("getRemoteResource: %w at %s", err, url)
 	}
+
 	return nil
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	err := getRemoteResource()
-
-	if err != nil {
-		fmt.Printf("error happend: %+v\n", err)
-		http.Error(w, "internal error", 500)
-		return
-	}
-
-	w.Write([]byte("all is OK"))
-}
-
-func main() {
-	http.HandleFunc("/", handler)
-	fmt.Println("starting server at :8080")
-	http.ListenAndServe(":8080", nil)
 }
--- 05_web_app/2_errors/3_own/own_err.go
+++ 05_web_app/2_errors/3_own/own_err.go
@@ -6,9 +6,7 @@ import (
 	"time"
 )
 
-var (
-	client = http.Client{Timeout: time.Duration(time.Millisecond)}
-)
+var client = http.Client{Timeout: time.Duration(time.Millisecond)}
 
 type ResourceError struct {
 	URL string
@@ -28,13 +26,11 @@ func (re *ResourceError) Unwrap() error {
 	return re.Err
 }
 
-func getRemoteResource() error {
-	url := "http://127.0.0.1:9999/pages?id=123"
-	_, err := client.Get(url)
-	if err != nil {
-		return &ResourceError{URL: url, Err: err}
-	}
-	return nil
+func main() {
+	http.HandleFunc("/", handler)
+
+	fmt.Println("starting server at :8080")
+	http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -49,13 +45,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 			fmt.Printf("internal error: %+v\n", err)
 			http.Error(w, "internal error", 500)
 		}
+
 		return
 	}
+
 	w.Write([]byte("all is OK"))
 }
 
-func main() {
-	http.HandleFunc("/", handler)
-	fmt.Println("starting server at :8080")
-	http.ListenAndServe(":8080", nil)
+func getRemoteResource() error {
+	url := "http://127.0.0.1:9999/pages?id=123"
+	_, err := client.Get(url)
+	if err != nil {
+		return &ResourceError{URL: url, Err: err}
+	}
+
+	return nil
 }
